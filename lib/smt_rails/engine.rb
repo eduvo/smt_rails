4cbf97ad8aacf03d3dd71bcdcb9806f5a907e9ea
Brandon Duval Benn <me@brandonbenn.xyz>
2022-07-29 16:03:07 +0800

    feat: check sprockets version

--- lib/smt_rails/engine.rb
+++ lib/smt_rails/engine.rb
@@ -8,9 +8,15 @@ module SmtRails
       app.config.assets.configure do |env|
         if env.respond_to?(:register_transformer)
           env.register_mime_type 'text/html', extensions: ['.mustache'], charset: :html
-          env.register_preprocessor 'text/html', Tilt, silence_deprecation: true
+          args = ['text/html', Tilt
+          args << { silence_deprecation: true } if Sprockets::VERSION.start_with?("3")
+          env.register_preprocessor *args
         elsif env.respond_to?(:register_engine)
-          env.register_engine(".#{SmtRails.template_extension}", Tilt)
+          args = [".#{SmtRails.template_extension}", Tilt]
+          if Sprockets::VERSION.start_with?("3")
+            args << { mime_type: 'text/html', extensions: ['.mustache'], silence_deprecation: true }
+          end
+          env.register_engine(*args)
         end
       end
       app.config.assets.paths << SmtRails.template_base_path
