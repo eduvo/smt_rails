c67d23c8b82ee7c3f5d2561ba54cb956b71df217
Brandon Duval Benn <me@brandonbenn.xyz>
2023-02-13 14:23:50 +0800

    update: tilt interface

--- lib/smt_rails/tilt.rb
+++ lib/smt_rails/tilt.rb
@@ -2,7 +2,7 @@ require 'tilt'
 
 module SmtRails
   class Tilt < Tilt::Template
-    self.default_mime_type = 'application/javascript'
+    self.metadata[:mime_type] = 'application/javascript'
 
     def prepare
       @namespace = "this.#{SmtRails.template_namespace}"
@@ -12,19 +12,23 @@ module SmtRails
 
     def evaluate(scope, locals, &block)
       template_key = path_to_key scope
-      <<-MustacheTemplate
-  (function() {
-  #{namespace} || (#{namespace} = {});
-  #{namespace}Cache || (#{namespace}Cache = {});
-  #{namespace}Cache[#{template_key.inspect}] = Mustache.compile(#{data.inspect});
-  Mustache.compilePartial(#{template_key.inspect}, #{data.inspect});
+      <<~MUSTACHE_TEMPLATE
+        (function() {
+        #{namespace} || (#{namespace} = {});
+        #{namespace}Cache || (#{namespace}Cache = {});
+        #{namespace}Cache[#{template_key.inspect}] = Mustache.compile(#{data.inspect});
+        Mustache.compilePartial(#{template_key.inspect}, #{data.inspect});
 
-  #{namespace}[#{template_key.inspect}] = function(object) {
-    if (!object){ object = {}; }
-    return #{SmtRails.template_namespace}Cache[#{template_key.inspect}](object);
-  };
-  }).call(this);
-      MustacheTemplate
+        #{namespace}[#{template_key.inspect}] = function(object) {
+          if (!object){ object = {}; }
+          return #{SmtRails.template_namespace}Cache[#{template_key.inspect}](object);
+        };
+        }).call(this);
+      MUSTACHE_TEMPLATE
+    end
+
+    def self.call(input)
+      new { input[:data] }.render
     end
 
     def path_to_key(scope)
